fca81bf3d8f0f2aff244dc60cc5cc32e06e42733
jasonxie <x5758x@gmail.com>
2022-08-13 16:22:06 +0800

    feat: add CORS env

--- src/core/settings/common.py
+++ src/core/settings/common.py
@@ -26,6 +26,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
+CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default=[])
 
 # Application definition
 
